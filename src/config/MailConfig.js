aee3e872fcd8197b87c3c449b8e133e39c4df9b6
Raynon Messias <59324178+raymessias@users.noreply.github.com>
2020-08-30 01:54:49 -0300

    Update MailConfig.js

--- src/config/MailConfig.js
+++ src/config/MailConfig.js
@@ -3,8 +3,8 @@ export default {
   port: 2525,
   secure: false,
   auth: {
-    user: '4d7ac954298243',
-    pass: '1cd54f882261f1'
+    user: '',
+    pass: ''
   },
   default: {
     from: 'Equipe GymPoint <noreply@gympoint.com>'
